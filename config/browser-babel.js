ee04729d82387a8013c553e0e17a7253d3d00fdc
Whizark <contact@whizark.com>
2016-05-04 02:35:50 +0900

    add browser-babel configuration for browser environment with Babel

---
+++ config/browser-babel.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    "extends": [
+        "./env/default.js",
+        "./env/browser.js",
+        "./version/es2015.js",
+        "./module/ecmascript.js",
+        "./rules/strict.js"
+    ]
+};
