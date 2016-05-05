b4090db5eb84f7839d6b262502647c0558b21729
Whizark <contact@whizark.com>
2016-05-05 22:20:59 +0900

    Revert "Revert "add node-babel configuration for Node.js environment with Babel""
    Actually, `module.exports` seems not to be needed.
    eslint-config is intended to lint the scripts that use ES2015 modules and
    the scripts built with Babel aren't the target.
    
    This reverts commit 84c6137ca50bcc80be26b4ae49dd68e027045232.

---
+++ config/node-babel.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    "extends": [
+        "./env/default.js",
+        "./env/node.js",
+        "./version/es2015.js",
+        "./module/ecmascript.js",
+        "./rules/strict.js"
+    ]
+};
