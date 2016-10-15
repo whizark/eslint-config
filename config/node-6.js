665986cb7cc52023d2afc00712599e5cd002e4e2
Whizark <contact@whizark.com>
2016-10-15 23:48:09 +0900

    add rules for Node.js 6

---
+++ config/node-6.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    "extends": [
+        "./env/default.js",
+        "./env/node.js",
+        "./version/es2015.js",
+        "./module/node.js",
+        "./rules/strict.js",
+        "./rules/node-6.js"
+    ]
+};
