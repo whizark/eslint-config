f635faa9f7d9565171348abd24b92589bfdb2d00
Whizark <contact@whizark.com>
2016-10-15 23:59:36 +0900

    add config for Node.js 7

---
+++ config/node-7.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    "extends": [
+        "./env/default.js",
+        "./env/node.js",
+        "./version/es2016.js",
+        "./module/node.js",
+        "./rules/strict.js",
+        "./rules/node-7.js"
+    ]
+};
---
+++ config/rules/node-7.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    "rules": {
+        "comma-dangle": [
+            "error",
+            {
+                "functions": "never"
+            }
+        ]
+    }
+};
