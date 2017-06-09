39ca70cf2c3246afad729f9ddd75c16767dbdfd5
Whizark <contact@whizark.com>
2017-06-10 07:06:51 +0900

    enable async related rules for Node.js 7
    Async/await has been supported by default since Node 7.6.0.

--- config/rules/node-7.js
+++ config/rules/node-7.js
@@ -2,9 +2,6 @@
 
 module.exports = {
     "rules": {
-        "no-await-in-loop": "off",
-        "no-return-await" : "off",
-        "require-await"   : "off",
         "comma-dangle"    : [
             "error",
             {
