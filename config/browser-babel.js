af486853a2d2d4c26dde118e1c15673c04e9329f
Whizark <contact@whizark.com>
2017-06-14 15:54:18 +0900

    use ES2017 parser for browser + babel environment

--- config/browser-babel.js
+++ config/browser-babel.js
@@ -4,7 +4,7 @@ module.exports = {
     "extends": [
         "./env/default.js",
         "./env/browser.js",
-        "./version/es2015.js",
+        "./version/es2017.js",
         "./module/ecmascript.js",
         "./rules/strict.js"
     ]
