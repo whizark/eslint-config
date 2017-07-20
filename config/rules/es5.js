c80f11d5c79817c3a32df8b9c3214bc5027bc42d
Whizark <contact@whizark.com>
2017-07-20 20:01:45 +0900

    explicitly turn off  rule for ES5

--- config/rules/es5.js
+++ config/rules/es5.js
@@ -2,13 +2,14 @@
 
 module.exports = {
     "rules": {
-        "no-await-in-loop"           : "off",
-        "no-template-curly-in-string": "off",
-        "class-methods-use-this"     : "off",
-        "no-empty-pattern"           : "off",
-        "no-return-await"            : "off",
-        "require-await"              : "off",
-        "comma-dangle"               : [
+        "no-await-in-loop"            : "off",
+        "no-template-curly-in-string" : "off",
+        "class-methods-use-this"      : "off",
+        "no-empty-pattern"            : "off",
+        "no-return-await"             : "off",
+        "require-await"               : "off",
+        "no-sync"                     : "off",
+        "comma-dangle"                : [
             "error",
             {
                 "arrays"   : "always-multiline",
@@ -18,30 +19,30 @@ module.exports = {
                 "functions": "never"
             }
         ],
-        "func-names"                 : [
+        "func-names"                  : [
             "error",
             "always"
         ],
-        "computed-property-spacing"  : "off",
-        "no-buffer-constructor"      : "off",
-        "arrow-body-style"           : "off",
-        "arrow-parens"               : "off",
-        "arrow-spacing"              : "off",
-        "constructor-super"          : "off",
-        "generator-star-spacing"     : "off",
-        "no-class-assign"            : "off",
-        "no-confusing-arrow"         : "off",
-        "no-const-assign"            : "off",
-        "no-dupe-class-members"      : "off",
-        "no-duplicate-imports"       : "off",
-        "no-new-symbol"              : "off",
-        "no-restricted-imports"      : "off",
-        "no-this-before-super"       : "off",
-        "no-useless-computed-key"    : "off",
-        "no-useless-constructor"     : "off",
-        "no-useless-rename"          : "off",
-        "no-var"                     : "off",
-        "object-shorthand"           : "off",
+        "computed-property-spacing"   : "off",
+        "no-buffer-constructor"       : "off",
+        "arrow-body-style"            : "off",
+        "arrow-parens"                : "off",
+        "arrow-spacing"               : "off",
+        "constructor-super"           : "off",
+        "generator-star-spacing"      : "off",
+        "no-class-assign"             : "off",
+        "no-confusing-arrow"          : "off",
+        "no-const-assign"             : "off",
+        "no-dupe-class-members"       : "off",
+        "no-duplicate-imports"        : "off",
+        "no-new-symbol"               : "off",
+        "no-restricted-imports"       : "off",
+        "no-this-before-super"        : "off",
+        "no-useless-computed-key"     : "off",
+        "no-useless-constructor"      : "off",
+        "no-useless-rename"           : "off",
+        "no-var"                      : "off",
+        "object-shorthand"            : "off",
         "prefer-arrow-callback"       : "off",
         "prefer-const"                : "off",
         "prefer-destructuring"        : "off",
@@ -58,4 +59,5 @@ module.exports = {
         "template-tag-spacing"        : "off",
         "yield-star-spacing"          : "off"
     }
-};
+}
+;
