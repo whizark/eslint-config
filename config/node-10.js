c61727374ed65cb20b498ef3b2d58a893603526d
Whizark <contact@whizark.com>
2018-04-27 05:02:12 +0900

    add config for Node 10

---
+++ config/node-10.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    "extends": [
+        "./env/default.js",
+        "./env/node.js",
+        "./version/es2018.js",
+        "./module/node.js",
+        "./rules/strict.js",
+        "./rules/node-10.js"
+    ]
+};
