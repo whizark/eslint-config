6cb94dbc51a7c7d3fd9d5ce9a93072e7e689d018
Whizark <contact@whizark.com>
2018-08-13 19:54:21 +0900

    relax `prefer-destructuring` rule

--- config/rules/strict.js
+++ config/rules/strict.js
@@ -1191,7 +1191,7 @@ module.exports = {
             }
         ],
         "prefer-destructuring"            : [
-            "error",
+            "warn",
             {
                 "VariableDeclarator"  : {
                     "array" : true,
