a1d1572951a41120d5ba9372e6a5fbf7b96e352f
Whizark <contact@whizark.com>
2019-11-13 03:15:38 +0900

    add new config for Node 13

---
+++ config/node-13.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    "extends": [
+        "./env/default.js",
+        "./env/node.js",
+        "./version/es2019.js",
+        "./module/node.js",
+        "./rules/strict.js",
+        "./rules/node-13.js"
+    ]
+};
