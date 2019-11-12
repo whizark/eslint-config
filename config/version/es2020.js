5b353a0fd3eaf4e6600979a1d3ac040be1dd3563
Whizark <contact@whizark.com>
2019-11-13 03:14:19 +0900

    add new version config for ECMAScript 2020
    eslint/eslint@9b86167e6f053e4a72bf68ebc79db53903f7f8c3

---
+++ config/version/es2020.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 2020
+    },
+    "env"          : {
+        "es6"   : true,
+        "es2017": true,
+        "es2020": true
+    }
+};
