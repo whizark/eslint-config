bd58dfe88b941418a6ed65cbffa578dfea003a12
Whizark <contact@whizark.com>
2020-08-21 06:29:32 +0900

    use ES2020 config for Node.js 14

--- config/node-14.js
+++ config/node-14.js
@@ -4,7 +4,7 @@ module.exports = {
     "extends": [
         "./env/default.js",
         "./env/node.js",
-        "./version/es2019.js",
+        "./version/es2020.js",
         "./module/node.js",
         "./rules/strict.js",
         "./rules/node-14.js"
